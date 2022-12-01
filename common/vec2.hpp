27f1177554a8e25ecdcf8afd7b0e92f714e784d9
CueXXIII <twitter.tv/cuexxiii>
2022-12-01 10:28:18 +0100

    use indentation width 4 by default

--- common/vec2.hpp
+++ common/vec2.hpp
@@ -3,46 +3,46 @@
 #include <fstream>
 
 template <typename num> struct Vec2 {
-  num x{};
-  num y{};
+    num x{};
+    num y{};
 
-  constexpr Vec2() noexcept = default;
-  constexpr Vec2(const num &x, const num &y) noexcept : x(x), y(y) {}
+    constexpr Vec2() noexcept = default;
+    constexpr Vec2(const num &x, const num &y) noexcept : x(x), y(y) {}
 
-  bool operator==(const Vec2 &other) const {
-    return x == other.x && y == other.y;
-  }
-  Vec2 &operator+=(const Vec2 &other) {
-    x += other.x;
-    y += other.y;
-    return *this;
-  }
-  Vec2 &operator-=(const Vec2 &other) {
-    x -= other.x;
-    y -= other.y;
-    return *this;
-  }
-  Vec2 &operator*=(const num factor) {
-    x *= factor;
-    y *= factor;
-    return *this;
-  }
+    bool operator==(const Vec2 &other) const {
+        return x == other.x && y == other.y;
+    }
+    Vec2 &operator+=(const Vec2 &other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+    Vec2 &operator-=(const Vec2 &other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
+    Vec2 &operator*=(const num factor) {
+        x *= factor;
+        y *= factor;
+        return *this;
+    }
 
-  Vec2 operator+(const Vec2 &other) const { return Vec2{*this} += other; }
-  Vec2 operator-(const Vec2 &other) const { return Vec2{*this} -= other; }
-  Vec2 operator*(const num factor) const { return Vec2{*this} *= factor; }
+    Vec2 operator+(const Vec2 &other) const { return Vec2{*this} += other; }
+    Vec2 operator-(const Vec2 &other) const { return Vec2{*this} -= other; }
+    Vec2 operator*(const num factor) const { return Vec2{*this} *= factor; }
 
-  friend std::ostream &operator<<(std::ostream &out, const Vec2 &vec) {
-    return out << "(" << vec.x << ", " << vec.y << ")";
-  }
+    friend std::ostream &operator<<(std::ostream &out, const Vec2 &vec) {
+        return out << "(" << vec.x << ", " << vec.y << ")";
+    }
 };
 
 template <typename num> struct std::hash<Vec2<num>> {
-  std::size_t operator()(const Vec2<num> &v) const noexcept {
-    return std::hash<int64_t>{}(
-        (static_cast<int64_t>(v.x) << 16 ^ static_cast<int64_t>(v.x) >> 48) ^
-        static_cast<int64_t>(v.y));
-  }
+    std::size_t operator()(const Vec2<num> &v) const noexcept {
+        return std::hash<int64_t>{}((static_cast<int64_t>(v.x) << 16 ^
+                                     static_cast<int64_t>(v.x) >> 48) ^
+                                    static_cast<int64_t>(v.y));
+    }
 };
 
 using Vec2i = Vec2<int32_t>;
--- common/vec3.hpp
+++ common/vec3.hpp
@@ -5,51 +5,51 @@
 #include "vec2.hpp"
 
 template <typename num> struct Vec3 {
-  num x{};
-  num y{};
-  num z{};
+    num x{};
+    num y{};
+    num z{};
 
-  constexpr Vec3() noexcept = default;
-  constexpr Vec3(const num &x, const num &y, const num &z) noexcept
-      : x(x), y(y), z(z) {}
-  constexpr Vec3(const Vec2<num> &v, num z) noexcept : x(v.x), y(v.y), z(z) {}
+    constexpr Vec3() noexcept = default;
+    constexpr Vec3(const num &x, const num &y, const num &z) noexcept
+        : x(x), y(y), z(z) {}
+    constexpr Vec3(const Vec2<num> &v, num z) noexcept : x(v.x), y(v.y), z(z) {}
 
-  bool operator==(const Vec3 &other) const {
-    return x == other.x && y == other.y && z == other.z;
-  }
-  Vec3 &operator+=(const Vec3 &other) {
-    x += other.x;
-    y += other.y;
-    z += other.z;
-    return *this;
-  }
-  Vec3 &operator-=(const Vec3 &other) {
-    x -= other.x;
-    y -= other.y;
-    z -= other.z;
-    return *this;
-  }
-  Vec3 &operator*=(const num factor) {
-    x *= factor;
-    y *= factor;
-    z *= factor;
-    return *this;
-  }
+    bool operator==(const Vec3 &other) const {
+        return x == other.x && y == other.y && z == other.z;
+    }
+    Vec3 &operator+=(const Vec3 &other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+    Vec3 &operator-=(const Vec3 &other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
+    Vec3 &operator*=(const num factor) {
+        x *= factor;
+        y *= factor;
+        z *= factor;
+        return *this;
+    }
 
-  Vec3 operator+(const Vec3 &other) const { return Vec3{*this} += other; }
-  Vec3 operator-(const Vec3 &other) const { return Vec3{*this} -= other; }
-  Vec3 operator*(const num factor) const { return Vec3{*this} *= factor; }
+    Vec3 operator+(const Vec3 &other) const { return Vec3{*this} += other; }
+    Vec3 operator-(const Vec3 &other) const { return Vec3{*this} -= other; }
+    Vec3 operator*(const num factor) const { return Vec3{*this} *= factor; }
 
-  friend std::ostream &operator<<(std::ostream &out, const Vec3 &vec) {
-    return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
-  }
+    friend std::ostream &operator<<(std::ostream &out, const Vec3 &vec) {
+        return out << "(" << vec.x << ", " << vec.y << ", " << vec.z << ")";
+    }
 };
 
 template <typename num> struct std::hash<Vec3<num>> {
-  std::size_t operator()(const Vec3<num> &v) const noexcept {
-    return std::hash<num>{}(v.x) * 2 + std::hash<num>{}(v.y) * 1627 +
-           std::hash<num>{}(v.z) * 2642257;
-  }
+    std::size_t operator()(const Vec3<num> &v) const noexcept {
+        return std::hash<num>{}(v.x) * 2 + std::hash<num>{}(v.y) * 1627 +
+               std::hash<num>{}(v.z) * 2642257;
+    }
 };
 
 using Vec3i = Vec3<int32_t>;
