9966d82ca2cbb7c6595438c87352c4f07c1cd24f
CueXXIII <twitter.tv/cuexxiii>
2022-12-01 07:42:41 +0100

    day 1 part 2 solution
    not the most efficient one, but fast to code

--- 01 - Calorie Counting/count.cc
+++ 01 - Calorie Counting/count.cc
@@ -1,10 +1,12 @@
+#include <algorithm>
 #include <fmt/format.h>
 #include <fstream>
 #include <string>
+#include <vector>
 
 int main(int, char **argv) {
     std::ifstream infile{argv[1]};
-    int64_t maxCalories = 0;
+    std::vector<int64_t> calories{};
     std::string line{};
     while (!infile.eof()) {
         int64_t currentCalories = 0;
@@ -15,10 +17,12 @@ int main(int, char **argv) {
             currentCalories += stol(line);
         }
         fmt::print("{}\n", currentCalories);
-        if (currentCalories > maxCalories) {
-            maxCalories = currentCalories;
-        }
+        calories.push_back(currentCalories);
     }
     fmt::print("\n");
-    fmt::print("Any elf carries at most {} calories.\n", maxCalories);
+    std::sort(calories.begin(), calories.end(),
+              [](auto a, auto b) { return a > b; });
+    fmt::print("Any elf carries at most {} calories.\n", calories[0]);
+    fmt::print("The top 3 elves carry a total of {} calories.\n",
+               calories[0] + calories[1] + calories[2]);
 }
