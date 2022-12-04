68711d835818e9d08aa774460382d2e242ccbcbe
CueXXIII <twitter.tv/cuexxiii>
2022-12-04 08:38:33 +0100

    day 4 part 2

--- 04 - Camp Cleanup/overlap.cc
+++ 04 - Camp Cleanup/overlap.cc
@@ -8,6 +8,7 @@ int main(int, char **argv) {
     std::ifstream infile{argv[1]};
     SimpleParser assigns{infile};
     int64_t overlapCounter = 0;
+    int64_t partialOverlaps = 0;
     while (!assigns.isEof()) {
         const auto range1s = assigns.getInt64();
         assigns.skipChar('-');
@@ -17,15 +18,21 @@ int main(int, char **argv) {
         assigns.skipChar('-');
         const auto range2e = assigns.getInt64();
 
+        // part 1
         if (range1s <= range2s and range1e >= range2e) {
-            fmt::print("{}-{} overlaps {}-{}\n", range2s, range2e, range1s,
-                       range1e);
             ++overlapCounter;
         } else if (range2s <= range1s and range2e >= range1e) {
-            fmt::print("{}-{} overlaps {}-{}\n", range1s, range1e, range2s,
-                       range2e);
             ++overlapCounter;
         }
+
+        // part 2
+        if ((range1s <= range2s and range2s <= range1e) or
+            (range1s <= range2e and range2e <= range1e) or
+            (range2s <= range1s and range2e >= range1e)) {
+            ++partialOverlaps;
+        }
     }
-    fmt::print("I have detected {} overlaps\n", overlapCounter);
+    fmt::print("I have detected {} total overlaps\n", overlapCounter);
+    fmt::print("And there are a total of {} partial overlaps\n",
+               partialOverlaps);
 }
