8a2e40fec85e363473be9b3a48de7cc804c7f500
CueXXIII <twitter.tv/cuexxiii>
2022-12-09 07:03:07 +0100

    day 9 part 2

--- 09 - Rope Bridge/mover.cc
+++ 09 - Rope Bridge/mover.cc
@@ -13,27 +13,31 @@
 
 using std::views::iota;
 
-std::unordered_set<Vec2l> visited{};
+std::unordered_set<Vec2l> visited1{};
+std::unordered_set<Vec2l> visited9{};
 
 const std::map<std::string, Vec2l> directions{
     {"R", {1, 0}}, {"L", {-1, 0}}, {"U", {0, -1}}, {"D", {0, 1}}};
 
 struct Rope {
     Vec2l head{0, 0};
-    Vec2l tail{0, 0};
+    std::vector<Vec2l> tails{};
 
-    Rope() { visited.insert(tail); }
+    Rope() {
+        tails.resize(9, {0, 0});
+        visited1.insert(tails[0]);
+        visited9.insert(tails[8]);
+    }
 
     template <typename T> T signum(const T num) {
         return (T{0} < num) - (num < T{0});
     }
 
-    void stepTail() {
+    void stepTail(const auto &head, auto &tail) {
         const auto difference = head - tail;
         if (std::abs(difference.x) > 1 or std::abs(difference.y) > 1) {
             const Vec2l moveStep = {signum(difference.x), signum(difference.y)};
             tail += moveStep;
-            visited.insert(tail);
         }
     }
 
@@ -41,7 +45,12 @@ struct Rope {
         const auto &direction = directions.at(directionName);
         for ([[maybe_unused]] const auto _ : iota(0, steps)) {
             head += direction;
-            stepTail();
+            stepTail(head, tails[0]);
+            visited1.insert(tails[0]);
+            for (const auto headNo : iota(0, 8)) {
+                stepTail(tails[headNo], tails[headNo + 1]);
+            }
+            visited9.insert(tails[8]);
         }
     }
 };
@@ -49,7 +58,7 @@ struct Rope {
 void printVisited() {
     for (const auto y : iota(-5, 2)) {
         for (const auto x : iota(-1, 7)) {
-            if (visited.contains({x, y})) {
+            if (visited1.contains({x, y})) {
                 fmt::print("#");
             } else {
                 fmt::print(".");
@@ -73,5 +82,6 @@ int main(int argc, char **argv) {
         const auto steps = scanner.getInt64();
         rope.move(direction, steps);
     }
-    fmt::print("The tail visited {} locations\n", visited.size());
+    fmt::print("The second knont visited {} locations\n", visited1.size());
+    fmt::print("The last knont visited {} locations\n", visited9.size());
 }
