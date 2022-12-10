dc4a8302baab6c04721ecd4a16c552d6610a0f0e
CueXXIII <twitter.tv/cuexxiii>
2022-12-10 06:32:38 +0100

    day 10 part 1

--- 10 - Cathode-Ray Tube/cpu.cc
+++ 10 - Cathode-Ray Tube/cpu.cc
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstdlib>
 #include <fmt/format.h>
 #include <fstream>
 #include <iostream>
@@ -10,18 +11,70 @@
 
 using std::views::iota;
 
+struct ALU {
+    SimpleParser commands;
+
+    int64_t cycle = 0;
+    int64_t x = 1;
+    enum num { empty, noop, addx } state = empty;
+    int64_t busy = 0;
+    int64_t parm = 0;
+
+    int64_t signal = 0;
+
+    ALU(char *filename) : commands(filename) {}
+
+    void fetchOp() {
+        if (commands.isEof()) {
+            fmt::print("The sum of all signals is {}\n", signal);
+            std::exit(EXIT_SUCCESS);
+        }
+        std::string opcode = commands.getToken();
+        if (opcode == "addx") {
+            state = addx;
+            busy = 2;
+            parm = commands.getInt64();
+            // fmt::print("{:3d}: {} {}\n", cycle + 1, opcode, parm);
+        } else if (opcode == "noop") {
+            state = noop;
+            busy = 1;
+            // fmt::print("{:3d}: {}\n", cycle + 1, opcode);
+        } else {
+            fmt::print("Invalid opcode {} error\n", opcode);
+            std::exit(EXIT_FAILURE);
+        }
+    }
+
+    void step() {
+        if (state == empty) {
+            fetchOp();
+        }
+        ++cycle;
+        if ((cycle == 20 or (cycle % 40) == 20) and cycle <= 220) {
+            signal += x * cycle;
+            fmt::print("Step {} has signal {}\n", cycle, x * cycle);
+        }
+        --busy;
+        if (busy == 0) {
+            if (state == addx) {
+                x += parm;
+            }
+            state = empty;
+        }
+    }
+    void loop() {
+        while (true) {
+            step();
+        }
+    }
+};
+
 int main(int argc, char **argv) {
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " <input.txt>\n";
         std::exit(EXIT_FAILURE);
     }
 
-    SimpleParser scanner{argv[1]};
-    while(!scanner.isEof()) {
-    }
-
-    std::ifstream infile{argv[1]};
-    std::string line;
-    while (std::getline(infile, line)) {
-    }
+    ALU alu{argv[1]};
+    alu.loop();
 }
