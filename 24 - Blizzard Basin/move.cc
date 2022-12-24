368243048cb3b5e35a5b9336f7506f8686649d31
CueXXIII <twitter.tv/cuexxiii>
2022-12-24 09:22:43 +0100

    day 24 cleanup
    I also had an error in the A* heuristic for the way back

--- 24 - Blizzard Basin/move.cc
+++ 24 - Blizzard Basin/move.cc
@@ -21,10 +21,8 @@ constexpr std::array<Vec2l, 5> direction{
 struct Plateau {
     int64_t width{};
     int64_t height{};
-    // can I move back to my start pos?
-    // yes, but that would be the same as just waiting there.
-    Vec2l startPos{0, -1};
-    Vec2l goalPos{};
+    Vec2l exitUp{};
+    Vec2l exitDown{};
 
     std::vector<bool> blizzUp{};
     std::vector<bool> blizzDown{};
@@ -88,7 +86,7 @@ struct Plateau {
                 std::cout << 'E';
                 continue;
             }
-            if (startPos.x != x) {
+            if (exitUp.x != x) {
                 std::cout << '#';
             } else {
                 std::cout << '.';
@@ -133,7 +131,7 @@ struct Plateau {
                 std::cout << 'E';
                 continue;
             }
-            if (goalPos.x != x) {
+            if (exitDown.x != x) {
                 std::cout << '#';
             } else {
                 std::cout << '.';
@@ -144,9 +142,6 @@ struct Plateau {
 
     // see if dest is free and reserve it. returns free state
     bool registerMove(const Vec2l &pos, const int64_t min) {
-        if (pos == startPos or pos == goalPos) {
-            return true;
-        }
         if (pos.x < 0 or pos.y < 0 or pos.x >= width or pos.y >= height) {
             return false;
         }
@@ -162,20 +157,21 @@ struct Plateau {
 
     // A*
     // move out, but not where we came in
-    int64_t findPath(const Vec2l &initialPos, int64_t startTime) {
+    int64_t findPath(const Vec2l &startPos, const Vec2l &endPos,
+                     int64_t startTime) {
         // int64_t singleStepCount{0}; // for debugging
         struct State {
             Vec2l pos;
             int64_t min;
         };
         auto compareState = [&](const State &a, const State &b) -> bool {
-            const auto aScore = a.min + manhattan(a.pos, goalPos);
-            const auto bScore = b.min + manhattan(b.pos, goalPos);
+            const auto aScore = a.min + manhattan(a.pos, endPos);
+            const auto bScore = b.min + manhattan(b.pos, endPos);
             return aScore > bScore;
         };
         std::priority_queue<State, std::vector<State>, decltype(compareState)>
             frontier{compareState};
-        frontier.emplace(initialPos, startTime);
+        frontier.emplace(startPos, startTime);
         visited.clear();
         visited.resize(width * height * blizzardCycle, false);
         while (!frontier.empty()) {
@@ -187,21 +183,19 @@ struct Plateau {
                 fmt::print("A*: step {} queuesize {}\n", singleStepCount,
             frontier.size()); print(current.pos, current.min);
             } */
-            if (current.pos == initialPos) {
+            if (current.pos == startPos) {
                 // wait, but not forever
                 if (current.min + 1 - startTime < blizzardCycle) {
                     frontier.emplace(current.pos, current.min + 1);
                 }
-            } else if (current.pos == startPos or current.pos == goalPos) {
-                return current.min;
             }
             for (const auto &dir : direction) {
-                if (current.pos + dir == initialPos) {
-                    // don't move back to the start, we can wait there
-                    continue;
+                const auto nextPos = current.pos + dir;
+                if (nextPos == endPos) {
+                    return current.min + 1;
                 }
-                if (registerMove(current.pos + dir, current.min + 1)) {
-                    frontier.emplace(current.pos + dir, current.min + 1);
+                if (registerMove(nextPos, current.min + 1)) {
+                    frontier.emplace(nextPos, current.min + 1);
                 }
             }
         }
@@ -222,7 +216,7 @@ int main(int argc, char **argv) {
     std::string line;
     std::getline(infile, line);
     plat.width = line.size() - 2;
-    plat.startPos = {static_cast<int64_t>(line.find('.')) - 1, -1};
+    plat.exitUp = {static_cast<int64_t>(line.find('.')) - 1, -1};
     while (std::getline(infile, line)) {
         if (line[1] == '#' or line[2] == '#') {
             break;
@@ -230,7 +224,7 @@ int main(int argc, char **argv) {
         winds.push_back(line);
     }
     plat.height = winds.size();
-    plat.goalPos = {static_cast<int64_t>(line.find('.')) - 1, plat.height};
+    plat.exitDown = {static_cast<int64_t>(line.find('.')) - 1, plat.height};
     plat.resize();
     for (const auto y : iota(0, plat.height)) {
         for (const auto x : iota(0, plat.width)) {
@@ -238,11 +232,11 @@ int main(int argc, char **argv) {
         }
     }
 
-    const auto way1 = plat.findPath(plat.startPos, 0);
+    const auto way1 = plat.findPath(plat.exitUp, plat.exitDown, 0);
     fmt::print("Reached the exit after {} minutes\n", way1);
     fmt::print("But we need to go back and get the snacks!\n");
-    const auto way2 = plat.findPath(plat.goalPos, way1);
+    const auto way2 = plat.findPath(plat.exitDown, plat.exitUp, way1);
     fmt::print("The snacks are still cold (minute {})\n", way2);
-    const auto way3 = plat.findPath(plat.startPos, way2);
+    const auto way3 = plat.findPath(plat.exitUp, plat.exitDown, way2);
     fmt::print("Now, that took {} minutes in the end\n", way3);
 }
