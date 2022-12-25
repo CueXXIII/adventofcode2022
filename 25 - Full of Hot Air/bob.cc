d2e49a72bbb008f6d8fcd8de973d9cf5890e73ce
CueXXIII <twitter.tv/cuexxiii>
2022-12-25 11:05:13 +0100

    day 25 put code into class SNAFU

--- 25 - Full of Hot Air/bob.cc
+++ 25 - Full of Hot Air/bob.cc
@@ -1,32 +1,62 @@
-#include <algorithm>
 #include <array>
 #include <fmt/format.h>
+#include <fmt/ostream.h>
 #include <fstream>
 #include <iostream>
-#include <ranges>
+#include <map>
 #include <string>
 #include <vector>
 
-using std::views::iota;
+struct SNAFU {
+    int64_t value;
 
-inline int64_t b5tob10(const char digit) {
-    switch (digit) {
-    case '0':
-    case '1':
-    case '2':
-        return digit - '0';
-    case '-':
-        return -1;
-    case '=':
-        return -2;
+    SNAFU(const std::string &from) {
+        int64_t base = 1;
+        value = 0;
+        // digit: DIGit ITerator
+        for (auto digit = from.rbegin(); digit != from.rend(); ++digit) {
+            const auto dig = [](const auto cell) {
+                switch (cell) {
+                case '-':
+                    return -1;
+                case '=':
+                    return -2;
+                default:
+                    return cell - '0';
+                }
+            }(*digit);
+            value += dig * base;
+            base *= 5;
+            if (base * 5 < base)
+                fmt::print("possible overflow at {}\n", from);
+        }
     }
-    return -666;
-}
+    SNAFU(const int64_t &from) : value(from) {}
 
-inline char b10to5(const int64_t digit) {
-    const static std::array<char, 5> d{{'=', '-', '0', '1', '2'}};
-    return d[digit + 2];
-}
+    SNAFU &operator+=(const SNAFU &other) {
+        value += other.value;
+        return *this;
+    }
+
+    friend std::ostream &operator<<(std::ostream &os, const SNAFU &number) {
+        const static std::array<char, 5> d{{'=', '-', '0', '1', '2'}};
+        std::vector<char> result{};
+        auto from = number.value;
+        while (from != 0) {
+            auto digit = from % 5;
+            if (digit > 2)
+                digit -= 5;
+            result.push_back(d.at(static_cast<size_t>(digit + 2)));
+            from -= digit;
+            from /= 5;
+        }
+        for (auto resit = result.rbegin(); resit != result.rend(); ++resit) {
+            os << *resit;
+        }
+        return os;
+    }
+};
+template <> struct fmt::formatter<SNAFU> : ostream_formatter {};
 
 int main(int argc, char **argv) {
     if (argc != 2) {
@@ -36,29 +66,11 @@ int main(int argc, char **argv) {
 
     std::ifstream infile{argv[1]};
     std::string line;
-    int64_t sum = 0;
+    SNAFU sum{0};
     while (std::getline(infile, line)) {
-        int64_t base = 1;
-        int64_t number = 0;
-        for (auto digit = line.rbegin(); digit != line.rend(); ++digit) {
-            const auto dig = b5tob10(*digit);
-            number += dig * base;
-            base *= 5;
-            if (base * 5 < base)
-                fmt::print("possible overflow at {}\n", line);
-        }
-        fmt::print("{}₅ = {}₁₀\n", line, number);
+        const SNAFU number{line};
+        fmt::print("{}₅ = {}₁₀\n", line, number.value);
         sum += number;
     }
-    const auto result10 = sum;
-    std::string result5{};
-    while (sum != 0) {
-        auto digit = sum % 5;
-        if (digit > 2)
-            digit -= 5;
-        result5 = b10to5(digit) + result5;
-        sum -= digit;
-        sum /= 5;
-    }
-    fmt::print("The sum is {}₁₀, or {}₅\n", result10, result5);
+    fmt::print("The sum is {}₁₀, or {}₅\n", sum.value, sum);
 }
